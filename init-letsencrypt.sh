d573a3841be8ca9d4783740a250bf86312a49bdc
Tomasz Budny <tbudny@fandom.com>
2021-11-14 15:41:46 +0100

    fix: init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(flashcards.pl www.flashcards.pl)
+domains=(flashclouds.pl www.flashclouds.pl)
 rsa_key_size=4096
 data_path="./local-dev/certbot"
 email="budny.tomasz@gmail.com" # Adding a valid address is strongly recommended
