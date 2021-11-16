065df9c0325f502f4a5fe6e032cf37298553f002
Tomasz Budny <tbudny@fandom.com>
2021-11-16 21:40:31 +0100

    feat: laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return 'Test route with Docker';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
